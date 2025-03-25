b4e875f2f9f86f2cbbba94a20eb7b348f08d7618
Chithritha V Kumar <chithritha.v.kumar@gmail.com>
2025-03-25 11:27:28 +0530

    opting streams

---
+++ prgm.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int choice;
+	printf("select your stream :\n);
+	printf("1.cse\n");
+	printf("2.aiml\n");
+	printf("3.ece\n");
+	printf("4.mech\n");
+	printf("enter your choice : ");
+	scanf("%d",&choice);
+	
+	switch (choice)
+	{
+		case 1:
+			printf("you selected cse.\n");
+			break;
+		case 2 :
+			printf("you selected aiml.\n");
+			break;
+		case 3:
+			printf("you selected ece.\n");
+			break;
+		case 4 :
+			printf("you selected mech.\n");
+			break;
+		default:
+			printf("invalid choice .\n");
+	}
+	return 0;
+}
+
